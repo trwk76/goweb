3a199cd236c6f485d38e79fa389f958d5994da30
Thomas KIPP <thomas.kipp@outlook.com>
2023-12-03 16:19:33 +0000

    contd

--- openapi/api.go
+++ openapi/api.go
@@ -1,21 +1,35 @@
 package openapi
 
-import "github.com/trwk76/goweb/openapi/spec"
+import (
+	"net/http"
 
-func New(path string, info spec.Info, mtyp MediaTypes) Builder {
+	"github.com/trwk76/goweb/openapi/spec"
+)
+
+func New[DR any](path string, info spec.Info, defResp DefaultResponder[DR]) Builder {
 	return Builder{
-		path: path,
-		info: info,
-		mtyp: mtyp,
+		Path:       path,
+		Info:       info,
+		MediaTypes: make(MediaTypes, 0),
+		Auths:      make(Auths, 0),
+		Responses:  newResponses(),
 	}
 }
 
 type (
+	DefaultResponder[T any] func(c *Context, status int) T
+
 	Builder struct {
 		Path       string
 		Info       spec.Info
 		MediaTypes MediaTypes
 		Auths      Auths
 		Responses  Responses
+
+		defResp func(c *Context, status int, w http.ResponseWriter)
+	}
+
+	Context struct {
+
 	}
 )
